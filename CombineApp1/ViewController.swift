ed772c98d7820ff89f61ad6b669e61218a09bc3a
Lammax <lammax@mail.ru>
2019-11-02 11:49:16 +0300

    Send SMS & get verification code form SMS

--- CombineApp1/ViewController.swift
+++ CombineApp1/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MessageUI
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var verifyCodeTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,8 +27,41 @@ class ViewController: UIViewController {
         
         center.removeObserver(notification)
         
+        if #available(iOS 12.0, *) {
+            self.verifyCodeTextField.textContentType = .oneTimeCode
+            self.verifyCodeTextField.keyboardType = .numberPad
+        }
+        
     }
     
-
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        let messageVC = MFMessageComposeViewController()
+        messageVC.body = "Verification code: 12345";
+        messageVC.recipients = ["89024672586"]
+        messageVC.messageComposeDelegate = self
+        self.present(messageVC, animated: true, completion: nil)
+    }
+    
+    
+    
 }
 
+extension ViewController: MFMessageComposeViewControllerDelegate {
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        switch (result) {
+        case .cancelled:
+            print("Message was cancelled")
+            dismiss(animated: true, completion: nil)
+        case .failed:
+            print("Message failed")
+            dismiss(animated: true, completion: nil)
+        case .sent:
+            print("Message was sent")
+            dismiss(animated: true, completion: nil)
+        default:
+            break
+        }
+    }
+    
+}
